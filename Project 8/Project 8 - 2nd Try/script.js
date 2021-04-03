735239f518abbb284a4463c2cc7bba7e5192bccb
robertmcshinsky <robert.mcshinsky@gmail.com>
2021-04-03 01:26:49 -0600

    added node and working on try 3

---
+++ Project 8/Project 8 - 2nd Try/script.js
@@ -0,0 +1,436 @@
+// TIME I HAVE WORKED ON THIS PROJECT -- ( 225 ) Minutes
+// Goal: 360 min totol
+// Time-left: 135 min -- 2 hr 15 min
+//DONE
+const months = [
+  {
+    month: "January",
+    days: 31,
+    holidays: [
+      {
+        name: "New Years",
+        dayOccurs: 1,
+      },
+      {
+        name: "Martin Luther King Jr. Day",
+        dayOccurs: 17,
+      },
+    ],
+  },
+  {
+    month: "February",
+    days: 28,
+    holidays: [
+      {
+        name: "Valentines Day",
+        dayOccurs: 14,
+      },
+      {
+        name: "Washington's Birthday",
+        dayOccurs: 21,
+      },
+    ],
+  },
+  {
+    month: "March",
+    days: 31,
+    holidays: [
+      {
+        name: "National Pie Day",
+        dayOccurs: 14,
+      },
+      {
+        name: "St. Patrick's Day",
+        dayOccurs: 17,
+      },
+    ],
+  },
+  {
+    month: "April",
+    days: 30,
+    holidays: [
+      {
+        name: "April Fools",
+        dayOccurs: 1,
+      },
+      {
+        name: "National Grilled Cheese Sandwich Day",
+        dayOccurs: 12,
+      },
+    ],
+  },
+  {
+    month: "May",
+    days: 31,
+    holidays: [
+      {
+        name: "Star Wars Day",
+        dayOccurs: 4,
+      },
+      {
+        name: "Memorial Day",
+        dayOccurs: 31,
+      },
+    ],
+  },
+  {
+    month: "June",
+    days: 30,
+    holidays: [
+      {
+        name: "D - Day",
+        dayOccurs: 6,
+      },
+      {
+        name: "National Kissing Day",
+        dayOccurs: 22,
+      },
+    ],
+  },
+  {
+    month: "July",
+    days: 31,
+    holidays: [
+      {
+        name: "Independence Day",
+        dayOccurs: 4,
+      },
+      {
+        name: "My Birthday",
+        dayOccurs: 17,
+      },
+    ],
+  },
+  {
+    month: "August",
+    days: 31,
+    holidays: [
+      {
+        name: "National Fajita Day",
+        dayOccurs: 18,
+      },
+    ],
+  },
+  {
+    month: "September",
+    days: 30,
+    holidays: [
+      {
+        name: "National Dance Day",
+        dayOccurs: 18,
+      },
+      {
+        name: "Batman Day",
+        dayOccurs: 21,
+      },
+    ],
+  },
+  {
+    month: "October",
+    days: 31,
+    holidays: [
+      {
+        name: "World Smile Day",
+        dayOccurs: 1,
+      },
+      {
+        name: "Eminem's Birthday",
+        dayOccurs: 17,
+      },
+    ],
+  },
+  {
+    month: "November",
+    days: 30,
+    holidays: [
+      {
+        name: "National Cancer Awareness Day",
+        dayOccurs: 7,
+      },
+      {
+        name: "Children's Day",
+        dayOccurs: 14,
+      },
+    ],
+  },
+  {
+    month: "December",
+    days: 31,
+    holidays: [
+      {
+        name: "Christmas",
+        dayOccurs: 25,
+      },
+      {
+        name: "New Years Eve",
+        dayOccurs: 31,
+      },
+    ],
+  },
+];
+
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let login = true;
+
+document.querySelector("#login").addEventListener("click", function () {
+  login = true;
+});
+let appts;
+console.log(week);
+console.log(months);
+
+// All time elements
+//DONE
+const date = new Date();
+let thisMonth = date.getMonth();
+let today = date.getDate();
+let weekDay = date.getDay();
+let year = date.getFullYear();
+let currentDay;
+
+function SetJumbotron() {
+  document.querySelector("#title").innerHTML = "Calendar";
+  document.querySelector("#monthName").innerHTML = months[thisMonth].month;
+  document.querySelector("#date").innerHTML = today;
+  document.querySelector("#login").innerHTML = "Login";
+}
+
+document.querySelector("#add").addEventListener("click", AddAppt);
+document.querySelector("#delete").addEventListener("click", DeleteAppt);
+SetJumbotron();
+Month();
+
+if (localStorage.getItem("Appointment") !== null) {
+  appts = JSON.parse(localStorage.getItem("Appointment"));
+  console.log(appts);
+  for (let h = 0; h < appts.length; ++h) {
+    let day = appts[h].day;
+
+    let prevAppt = document.createElement("div");
+    prevAppt.setAttribute("id", "Day" + day + "SubContent");
+    prevAppt.setAttribute("class", "content");
+
+    document
+      .querySelector("#Day" + day + "Content")
+      .setAttribute("style", "display: none");
+
+    prevAppt.innerHTML = "- " + appts[h].info;
+    document.querySelector("#Day" + day + "Content").appendChild(prevAppt);
+
+    document
+      .querySelector("#Day" + day)
+      .setAttribute("onmouseover", "show(this.id)");
+    document
+      .querySelector("#Day" + day)
+      .setAttribute("onmouseout", "remove(this.id)");
+  }
+} else if (localStorage.getItem("Appointment") === null) {
+  appts = new Array(0);
+}
+//DONE
+
+//
+// Create the days inside each month
+//DONE
+function Month() {
+  let month = document.querySelector("#month");
+  let weekNames = document.querySelector("#weekNames");
+
+  // Created the week names
+  for (let i = 0; i < 7; ++i) {
+    let weekday = document.createElement("div");
+    weekday.setAttribute("class", "weekday");
+    weekday.innerHTML = week[i];
+    weekNames.appendChild(weekday);
+  }
+
+  for (let k = 0; k < months[thisMonth].days; ++k) {
+    let day = document.createElement("div");
+    let content = document.createElement("div");
+
+    day.classList.add("day");
+    day.classList.add("empty");
+    day.setAttribute("id", "Day" + (k + 1));
+    day.innerHTML = k + 1;
+
+    content.setAttribute("id", "Day" + (k + 1) + "Content");
+    content.classList.add("content");
+
+    for (let j = 0; j < months[thisMonth].holidays.length; ++j) {
+      if (months[thisMonth].holidays[j].dayOccurs === k + 1) {
+        content.innerHTML = " - " + months[thisMonth].holidays[j].name;
+        day.setAttribute("onmouseover", "show(this.id)");
+        day.setAttribute("onmouseout", "remove(this.id)");
+        day.classList.remove("empty");
+        day.classList.add("full");
+        content.style.display = "none";
+        break;
+      }
+    }
+    day.appendChild(content);
+    month.appendChild(day);
+  }
+}
+
+function clearOptions() {
+  document.querySelector("#options").innerHTML = "";
+}
+
+//TODO
+function AddAppt() {
+  clearOptions();
+
+  if (login) {
+    console.log("Logged In");
+  } else {
+    alert("Please Login in to add an event.");
+    return 0;
+  }
+
+  console.log("AddAppt");
+  let selection = document.createElement("div");
+  selection.setAttribute("id", "selection");
+  selection.setAttribute("class", "selection");
+
+  let form = document.createElement("form");
+  form.setAttribute("class", "form");
+
+  let labelInfo = document.createElement("label");
+  labelInfo.setAttribute("for", "info");
+  labelInfo.innerHTML = "What do you want to put in your schedule?";
+
+  let inputInfo = document.createElement("input");
+  inputInfo.setAttribute("name", "info");
+  inputInfo.setAttribute("type", "text");
+  inputInfo.setAttribute("id", "info");
+
+  let labelDate = document.createElement("div");
+  labelDate.setAttribute("for", "date");
+  labelDate.innerHTML = "What day should we put this in?";
+
+  let selectDate = document.createElement("select");
+  selectDate.setAttribute("id", "date");
+  selectDate.setAttribute("name", "date");
+  selectDate.setAttribute("onChange", "myNewFunction(this)");
+
+  for (let i = 0; i < months[thisMonth].days; ++i) {
+    let option = document.createElement("option");
+    option.setAttribute("value", i + 1);
+    option.innerHTML = i + 1;
+
+    selectDate.appendChild(option);
+  }
+
+  let submit = document.createElement("div");
+  submit.setAttribute("id", "submit");
+  submit.setAttribute("class", "Submit");
+  submit.innerHTML = "Submit";
+  submit.setAttribute("onClick", "addToCal()");
+
+  form.appendChild(labelInfo);
+  form.appendChild(inputInfo);
+  form.appendChild(labelDate);
+  form.appendChild(selectDate);
+  form.appendChild(submit);
+
+  document.querySelector("#options").appendChild(form);
+}
+
+//TODO
+function DeleteAppt() {
+  console.log("DeleteAppt");
+  clearOptions();
+  let selection = document.createElement("div");
+  selection.setAttribute("id", "selection");
+  selection.setAttribute("class", "selection");
+
+  let form = document.createElement("form");
+  form.setAttribute("class", "form");
+
+  for (let i = 0; i < appts.length; ++i) {
+    let deleteContainer = document.createElement("div");
+    deleteContainer.setAttribute("id", "checkbox" + i + "del");
+    deleteContainer.setAttribute("class", "delCon");
+
+    let delButton = document.createElement("button");
+    delButton.setAttribute("id", "checkbox" + i);
+    delButton.setAttribute("class", "checkbox");
+    delButton.setAttribute("onClick", "delFromCal(this.id)");
+    delButton.innerHTML = "Delete Event";
+
+    let cbxLabel = document.createElement("label");
+    cbxLabel.setAttribute("for", "appt" + i);
+    cbxLabel.innerHTML =
+      appts[i].month + " " + appts[i].day + " ➡ " + appts[i].info + "<br>";
+
+    deleteContainer.appendChild(delButton);
+    deleteContainer.appendChild(cbxLabel);
+    form.appendChild(deleteContainer);
+  }
+
+  let submit = document.createElement("div");
+  submit.setAttribute("id", "submitD");
+  submit.setAttribute("class", "SubmitD");
+  submit.innerHTML = "Delete";
+
+  form.appendChild(submit);
+
+  document.querySelector("#options").appendChild(form);
+}
+
+function show(thisId) {
+  document.getElementById(thisId + "Content").style.display = "block";
+}
+function remove(thisId) {
+  document.getElementById(thisId + "Content").style.display = "none";
+}
+
+function addToCal() {
+  console.log("@addToCal");
+  let info = document.querySelector("#info").value;
+  console.log();
+
+  appts.push({ info: info, month: months[thisMonth].month, day: currentDay });
+
+  let myJSON = JSON.stringify(appts);
+  localStorage.setItem("Appointment", myJSON);
+
+  let newAppt = document.createElement("div");
+  newAppt.setAttribute("id", "Day" + currentDay + "SubContent");
+  newAppt.setAttribute("class", "content");
+
+  document
+    .querySelector("#Day" + currentDay + "Content")
+    .setAttribute("style", "display: none");
+
+  newAppt.innerHTML = "- " + info;
+  console.log(newAppt);
+  document.querySelector("#Day" + currentDay + "Content").appendChild(newAppt);
+
+  document
+    .querySelector("#Day" + currentDay)
+    .setAttribute("onmouseover", "show(this.id)");
+  document
+    .querySelector("#Day" + currentDay)
+    .setAttribute("onmouseout", "remove(this.id)");
+}
+
+function delFromCal(id) {
+  console.log("@delFromCal");
+  console.log(id);
+  document.querySelector("#" + id).remove();
+  document.querySelector("#" + id + "del").remove();
+}
+
+function myNewFunction(sel) {
+  currentDay = sel.options[sel.selectedIndex].text;
+}
--- Project 8/Project 8 - Try 3/script.js
+++ Project 8/Project 8 - Try 3/script.js
@@ -1,16 +1,200 @@
-const Year = require("./year.json");
+const Year = {
+  Months: [
+    {
+      name: "January",
+      days: "31",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "February",
+      days: "30",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "March",
+      days: "31",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "April",
+      days: "30",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "May",
+      days: "31",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "June",
+      days: "30",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "July",
+      days: "31",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "August",
+      days: "30",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "September",
+      days: "31",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "October",
+      days: "30",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "November",
+      days: "31",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+    {
+      name: "December",
+      days: "30",
+      holidays: [
+        {
+          name: "ABC holiday",
+          date: 10,
+        },
+        {
+          name: "DEF holiday",
+          date: 20,
+        },
+      ],
+    },
+  ],
+  Week: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+};
 
 class Appointment {
-  constructor(year, month, day) {
+  constructor(year, month, day, info, id) {
     this.year = year;
     this.month = month;
     this.day = day;
+    this.info = info;
+    this.id = id;
   }
   setAppointment() {
     let appointment = {
       year: this.year,
       month: this.month,
       day: this.day,
+      info: this.info,
+      id: this.id,
     };
     console.log(appointment);
   }
