ed3bf2901e11189df2d1f916c6dd2b164800f516
robertmcshinsky <robert.mcshinsky@gmail.com>
2021-04-05 16:56:16 -0600

    Created nice month, as well as started on the days functions... Also got the appointments with local storage to work

--- Project 8/Project 8 - Try 3/script.js
+++ Project 8/Project 8 - Try 3/script.js
@@ -1,3 +1,5 @@
+//const { stringify } = require("qs");
+
 const Year = {
   Months: [
     {
@@ -179,7 +181,23 @@ const Year = {
     "Saturday",
   ],
 };
+let date = new Date();
+let day = date.getDate();
+let month = Year.Months[date.getMonth()].name;
+let year = date.getFullYear();
 
+let fullDate = day + " / " + month + " / " + year;
+
+document.querySelector("#fullDate").innerHTML = fullDate;
+
+let apptList;
+let account = [];
+if (JSON.parse(localStorage.getItem("Appointments")) === "") {
+  apptList = JSON.parse(localStorage.getItem("Appointments"));
+  console.log(appList);
+} else {
+  apptList = [];
+}
 class Appointment {
   constructor(year, month, day, info, id) {
     this.year = year;
@@ -188,7 +206,7 @@ class Appointment {
     this.info = info;
     this.id = id;
   }
-  setAppointment() {
+  makeAppointment() {
     let appointment = {
       year: this.year,
       month: this.month,
@@ -196,8 +214,111 @@ class Appointment {
       info: this.info,
       id: this.id,
     };
+
     console.log(appointment);
+    apptList.push(appointment);
+    localStorage.setItem("Appointments", JSON.stringify(apptList));
   }
 }
 
-let newCal = new Appointment(2021, "Jan", 23).setAppointment();
+class Login {
+  constructor(userName, password) {
+    this.userName = userName;
+    this.password = password;
+  }
+}
+
+class SignUp {
+  constructor(firstName, lastName, userName, password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userName = userName;
+    this.password = password;
+  }
+}
+class Calendar {
+  constructor(year, month, days, appointments, holidays) {
+    this.year = year;
+    this.month = month;
+    this.days = days;
+    this.appointments = appointments;
+    this.holidays = holidays;
+  }
+
+  createCalendar() {
+    let calendar, weekDays, cardColumns;
+
+    // Creating the Elements
+    calendar = document.createElement("div");
+    weekDays = document.createElement("div");
+    cardColumns = document.createElement("div");
+
+    // Setting the attributes for each Element
+    calendar.setAttribute("class", "calendar");
+    calendar.setAttribute("style", "display: block");
+    weekDays.setAttribute("class", "weekDays");
+    cardColumns.setAttribute("class", "card-columns");
+    cardColumns.setAttribute(
+      "style",
+      "column-count: 7; width: 90%; margin-left: 5%"
+    );
+
+    // Creating each Weekday Card
+    for (let i = 0; i < 7; ++i) {
+      let card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      let cardBody = document.createElement("div");
+      cardBody.setAttribute("class", "card-body text-center");
+
+      let cardTitle = document.createElement("h5");
+      cardTitle.setAttribute("class", "card-title");
+      cardTitle.innerHTML = Year.Week[i];
+
+      cardBody.appendChild(cardTitle);
+      card.appendChild(cardBody);
+      cardColumns.appendChild(card);
+    }
+
+    weekDays.appendChild(cardColumns);
+
+    /*-------------------------------*/
+    let everyDay, cardColumnsDays;
+
+    everyDay = document.createElement("div");
+    cardColumnsDays = document.createElement("div");
+
+    everyDay.setAttribute("class", "everyDay");
+    cardColumnsDays.setAttribute("class", "card-column d-flex");
+    cardColumnsDays.setAttribute(
+      "style",
+      "column-count: 7; width: 90%; margin-left: 5%"
+    );
+
+    for (let k = 0; k < 7; ++k) {
+      let cardDay = document.createElement("div");
+      cardDay.setAttribute("class", "card text-center");
+      cardDay.setAttribute("style", "width: 13%; margin-left: 1.1%");
+
+      let cardBodyDay = document.createElement("div");
+      cardBodyDay.setAttribute("class", "card-body ");
+
+      let cardTitleDay = document.createElement("h5");
+      cardTitleDay.setAttribute("class", "card-title");
+      cardTitleDay.innerHTML = k + 1;
+
+      cardBodyDay.appendChild(cardTitleDay);
+      cardDay.appendChild(cardBodyDay);
+      cardColumnsDays.appendChild(cardDay);
+    }
+    everyDay.appendChild(cardColumnsDays);
+
+    calendar.appendChild(weekDays);
+    calendar.appendChild(everyDay);
+    document.querySelector("#main").appendChild(calendar);
+  }
+
+  setAppointments() {}
+}
+
+let calendar = new Calendar().createCalendar();
